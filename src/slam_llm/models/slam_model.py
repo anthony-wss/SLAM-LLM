Fix the model loading precision

src/slam_llm/models/slam_model.py
@@ -182,7 +182,7 @@ def setup_llm(train_config, model_config, **kwargs):
             if train_config.quantization:
                 load_kwargs["load_in_8bit"] = True
                 load_kwargs["device_map"] = "auto"
-            if train_config.use_fp16:
+            if train_config.use_fp16 or getattr(train_config, "mixed_precision", False):
                 load_kwargs["torch_dtype"] = torch.float32
             model = AutoModelForCausalLM.from_pretrained(
                 model_config.llm_path,